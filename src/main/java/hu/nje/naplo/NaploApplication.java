8dff9c6c7d84e1f1d9f0af0038e3509887f02439
balogh.norbert <balogh.norbert92@gmail.com>
2024-10-09 15:34:57 +0200

    init commit

---
+++ src/main/java/hu/nje/naplo/NaploApplication.java
@@ -0,0 +1,13 @@
+package hu.nje.naplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaploApplication.class, args);
+	}
+
+}
