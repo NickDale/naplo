34764a735ad1f35a5702362f9e8bc16df3097d94
NickDale <balogh.norbert92@gmail.com>
2024-11-30 22:01:53 +0100

    unused clean

--- src/main/java/hu/nje/naplo/repository/SubjectRepository.java
+++ src/main/java/hu/nje/naplo/repository/SubjectRepository.java
@@ -8,14 +8,6 @@ import java.util.List;
 
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
 //    List<Subject> findByCategory(String category);
-//
-//    void addSubject(String subjectName, String category);
-//
-//    Object getCategories();
-//
-//    void deleteStudentById(@Positive int studentId);
-//
-//    List<?> listSubjects();
-//    boolean existsByName(String name);
+
 //    boolean existsByName(String name);
 }
